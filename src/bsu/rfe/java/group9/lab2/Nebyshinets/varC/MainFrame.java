f23a88bb37e87525d9bbb7939a15533a5f6115d8
stas <stas.v.nebyshinets@gmail.com>
2020-12-08 15:14:05 +0300

    made code more readable

--- src/bsu/rfe/java/group9/lab2/Nebyshinets/varC/MainFrame.java
+++ src/bsu/rfe/java/group9/lab2/Nebyshinets/varC/MainFrame.java
@@ -29,6 +29,7 @@ public class MainFrame extends JFrame {
     private JCheckBoxMenuItem showTicksMenuItem;
     private GraphicsDisplay display = new GraphicsDisplay();
     private boolean fileLoaded = false;
+
     public MainFrame() {
         super("Построение графиков функций на основе заранее подготовленных файлов");
         setSize(WIDTH, HEIGHT);
@@ -84,74 +85,56 @@ public class MainFrame extends JFrame {
 
         graphicsMenu.addMenuListener(new GraphicsMenuListener());
         getContentPane().add(display, BorderLayout.CENTER);
-        }
-// Считывание данных графика из существующего файла
-protected void openGraphics(File selectedFile) {
+    }
+
+    protected void openGraphics(File selectedFile) {
         try {
-// Шаг 1 - Открыть поток чтения данных, связанный с входным
         DataInputStream in = new DataInputStream(new
         FileInputStream(selectedFile));
-/* Шаг 2 - Зная объѐм данных в потоке ввода можно вычислить,
-* сколько памяти нужно зарезервировать в массиве:
-* Всего байт в потоке - in.available() байт;
-* Размер одного числа Double - Double.SIZE бит, или
-Double.SIZE/8 байт;
-* Так как числа записываются парами, то число пар меньше в
-2 раза
-*/
+
         Double[][] graphicsData = new
         Double[in.available()/(Double.SIZE/8)/2][];
-// Шаг 3 - Цикл чтения данных (пока в потоке есть данные)
+
         int i = 0;
         while (in.available()>0) {
-// Первой из потока читается координата точки X
-        Double x = in.readDouble();
-// Затем - значение графика Y в точке X
-        Double y = in.readDouble();
-// Прочитанная пара координат добавляется в массив
-        graphicsData[i++] = new Double[] {x, y};
+            Double x = in.readDouble();
+            Double y = in.readDouble();
+            graphicsData[i++] = new Double[] {x, y};
         }
-// Шаг 4 - Проверка, имеется ли в списке в результате чтения
         if (graphicsData!=null && graphicsData.length>0) {
-// Да - установить флаг загруженности данных
-        fileLoaded = true;
-// Вызывать метод отображения графика
-        display.showGraphics(graphicsData);
+            fileLoaded = true;
+            display.showGraphics(graphicsData);
         }
-// Шаг 5 - Закрыть входной поток
         in.close();
         } catch (FileNotFoundException ex) {
-// В случае исключительной ситуации типа "Файл не найден"
-        JOptionPane.showMessageDialog(MainFrame.this, "Указанный " +
+            JOptionPane.showMessageDialog(MainFrame.this, "Указанный " +
                 "файл не найден", "Ошибка загрузки данных", JOptionPane.WARNING_MESSAGE);
-        return;
+            return;
         } catch (IOException ex) {
-// В случае ошибки ввода из файлового потока показать
-        JOptionPane.showMessageDialog(MainFrame.this, "Ошибка чтения " +
+            JOptionPane.showMessageDialog(MainFrame.this, "Ошибка чтения " +
                         "координат точек из файла", "Ошибка загрузки данных",
-        JOptionPane.WARNING_MESSAGE);
-        return;
+                        JOptionPane.WARNING_MESSAGE);
+            return;
         }
-        }
-public static void main(String[] args) {
-// Создать и показать экземпляр главного окна приложения
+    }
+
+    public static void main(String[] args) {
         MainFrame frame = new MainFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    private class GraphicsMenuListener implements MenuListener {
+
+        public void menuSelected(MenuEvent e) {
+            showAxisMenuItem.setEnabled(fileLoaded);
+            showMarkersMenuItem.setEnabled(fileLoaded);
         }
-// Класс-слушатель событий, связанных с отображением меню
-private class GraphicsMenuListener implements MenuListener {
-    // Обработчик, вызываемый перед показом меню
-    public void menuSelected(MenuEvent e) {
-// Доступность или недоступность элементов меню "График"
-        showAxisMenuItem.setEnabled(fileLoaded);
-        showMarkersMenuItem.setEnabled(fileLoaded);
+
+        public void menuDeselected(MenuEvent e) {
+        }
+
+        public void menuCanceled(MenuEvent e) {
+        }
     }
-    // Обработчик, вызываемый после того, как меню исчезло с экрана
-    public void menuDeselected(MenuEvent e) {
-    }
-// Обработчик, вызываемый в случае отмены выбора пункта меню
-    public void menuCanceled(MenuEvent e) {
-    }
-}
 }
